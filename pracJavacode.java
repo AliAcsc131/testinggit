902e69440a6258a2f90a43c8e79a3c4efb056f14
AliAcsc131 <aliataya@csus.edu>
2019-11-01 14:06:39 -0700

    added code:

---
+++ pracJavacode.java
@@ -0,0 +1,32 @@
+class pracJavacode {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 6;
+		
+		
+		System.out.println(multiply(x,y));
+		
+		
+		
+		
+		
+	}
+	
+	static int multiply(int x, int y) {
+		
+		return x*y;
+		
+		
+	}
+	
+	
+	
+	static int divide(int x, int y) {
+		
+		return x+y;
+		
+		
+	}
+	
+	
+}
